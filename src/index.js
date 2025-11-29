a8149beaa15dd81a0330e7a2f9c6a42a76ea8d75
Tin <martin.simbana007@gmail.com>
2025-11-29 14:29:39 -0500

    refactor: clean up index.js and improve code formatting

--- src/index.js
+++ src/index.js
@@ -1,27 +1,63 @@
-require('dotenv').config();
-// 💡 Nueva importación
-const cors = require('cors'); 
-const tareaRoutes = require('./routes/tareaRoutes');
-// ... (otras importaciones)
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+const cors = require("cors");
+const tareaRoutes = require("./routes/tareaRoutes");
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const { apiReference } = require('@scalar/express-api-reference');
+const { apiReference } = require("@scalar/express-api-reference");
 
 const port = process.env.PORT;
 
-const allowedOrigin = process.env.CORS_ORIGIN || 'http://localhost:4200';
+const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:4200";
 
 const corsOptions = {
-  origin: allowedOrigin,
-  credentials: true
+  origin: allowedOrigin,
+  credentials: true,
 };
 
-
-app.use(cors(corsOptions)); 
-
-
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
+
+console.log("puerto", port);
+
+app.get("/", (req, res) => {
+  res.send("API backend funcionando ");
+});
+
+app.use("/api", tareaRoutes);
+app.use("/api", authRoutes);
+
+app.use(
+  "/docs",
+  apiReference({
+    theme: "purple",
+    layout: "moderm",
+    spec: {
+      url: "/api/openapi.yaml",
+    },
+    configuration: {
+      showSidebar: true,
+      hideDownloadButton: false,
+      hideTryItPanel: false,
+      authentication: {
+        preferredSecurityScheme: "bearerAuth",
+        apiKey: {
+          token: "token",
+        },
+      },
+    },
+  })
+);
+
+app.get("/api/openapi.yaml", (req, res) => {
+  res.setHeader("Content-Type", "application/x-yaml");
+  res.sendFile(path.join(__dirname, "../docs/openapi.yaml"));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+  console.log(`Documentación disponible en: http://localhost:${port}/docs`);
+});
